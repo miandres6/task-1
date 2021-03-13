66eee80eb59829f4b944e057ded0cce4e335c46d
miandres6 <ma.castillo1@uniandes.edu.co>
2021-03-12 22:24:21 -0500

    Faltan las graficas

--- Solution.R
+++ Solution.R
@@ -63,13 +63,48 @@ pacman::p_load(tidyverse,readxl,haven)
 caracteristicas_generales=readRDS(file='data/input/2019/Cabecera - Caracteristicas generales (Personas).rds')
 ocupados=readRDS(file='data/input/2019/Cabecera - Ocupados.rds') 
 
+duplicated(caracteristicas_generales$directorio)%>% table()
+duplicated(paste0(caracteristicas_generales$directorio, caracteristicas_generales$secuencia_p))%>% table()
+duplicated(paste0(caracteristicas_generales$directorio, caracteristicas_generales$secuencia_p, caracteristicas_generales$orden))%>% table(
+  
+) #No existen individuos duplicados en caracteristicas_generales
+
+duplicated(paste0(ocupados$directorio,ocupados$secuencia_p,ocupados$orden)) %>% table() #Ocupados son identificadores unicos
+
+
 #Unir base de datos en una sola
 nueva_base=full_join(caracteristicas_generales,ocupados,by=c('directorio','secuencia_p','orden'))
 View(nueva_base)
 
-#Descriptivas
-
+nueva_base$Ocupados=ifelse(is.na(nueva_base$mes.y), 0, 1) #desempleados=0
 
 #Guardar la base
 saveRDS(object = nueva_base , file = "data/output/NuevaBase.rds")
+
+#3.2 Descriptivas
+
+summary(nueva_base)
+
+#Número de ocupados:
+#-Por edad
+nueva_base %>%  group_by(P6040)%>% summarise(media=mean(Ocupados), varz=var(Ocupados),dsvest=sd(Ocupados), total=sum(Ocupados))
+#-Por sexo
+nueva_base %>% group_by(P6020) %>%  summarise(media=mean(Ocupados), varz=var(Ocupados),dsvest=sd(Ocupados), total=sum(Ocupados))
+#-Por tipo de contrato
+nueva_base %>% group_by(P6430) %>%  summarise(media=mean(Ocupados), varz=var(Ocupados),dsvest=sd(Ocupados), total=sum(Ocupados))
+#-Urbano/Rural  
+nueva_base %>% group_by(area.x) %>% summarise(media=mean(Ocupados), varz=var(Ocupados),dsvest=sd(Ocupados), total=sum(Ocupados))
+
+#Ingresos laborales:
+a=subset(nueva_base,is.na(P6750)==F)
+
+#-Por edad
+a %>%  group_by(P6040)%>% summarise(media=mean(P6750), varz=var(P6750), dsvest=sd(P6750),total=sum(P6750))
+#-Por sexo
+a %>% group_by(P6020) %>% summarise(media=mean(P6750), varz=var(P6750), dsvest=sd(P6750),total=sum(P6750))
+#-Por tipo de contrato
+a %>% group_by(P6430) %>% summarise(media=mean(P6750), varz=var(P6750), dsvest=sd(P6750),total=sum(P6750))
+#-Urbano/Rural  
+a %>% group_by(area.x) %>% summarise(media=mean(P6750), varz=var(P6750), dsvest=sd(P6750),total=sum(P6750))
+
 
